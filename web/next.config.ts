db0282735edde1107db9d2726f4ef8c0f6e0b138
Utsav Jana <utsav.jana.5@gmail.com>
2026-07-14 12:34:28 +0530

    fix next.config.ts to set turbopack root

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopack: {
+      // Force Turbopack to resolve relative to this project folder
+      root: path.resolve("."),
+    },
+  },
 };
 
 export default nextConfig;
